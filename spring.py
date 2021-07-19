b9d861f0ced3a9805eb035436ef7cb59303f0fa6
Lucius Bligh <lucius@lucius.org.uk>
2021-07-19 16:05:53 +0100

    added verlet and analytical solutions

--- spring.py
+++ spring.py
@@ -3,43 +3,93 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# mass, spring constant, initial position and velocity
-m = 1
-k = 1
-x = 0
-v = 1
 
-# simulation time, timestep and time
-t_max = 100
-dt = 0.1
-t_array = np.arange(0, t_max, dt)
+def main():
 
-# initialise empty lists to record trajectories
-x_list = []
-v_list = []
+    # mass, spring constant, initial positions and velocities
+    m = 1
+    k = 1
+    x_euler = x_verlet = 0
+    v_euler = v_verlet = 1
 
-# Euler integration
-for t in t_array:
+    # simulation time, timestep and time
+    t_max = 100
+    dt = 0.1
+    t_array = np.arange(0, t_max, dt)
 
-    # append current state to trajectories
-    x_list.append(x)
-    v_list.append(v)
+    # analytical constants
+    omega = np.sqrt(k/m)
+    B = 1/omega
 
-    # calculate new position and velocity
-    a = -k * x / m
-    x = x + dt * v
-    v = v + dt * a
+    # initialise empty lists to record trajectories
+    euler_x_list = []
+    euler_v_list = []
+    verlet_x_list = []
 
-# convert trajectory lists into arrays, so they can be sliced (useful for Assignment 2)
-x_array = np.array(x_list)
-v_array = np.array(v_list)
+    # initialise verlet velocity list - handled differently
+    verlet_v_list = [v_verlet]
 
-# plot the position-time graph
-plt.figure(1)
-plt.clf()
-plt.xlabel('time (s)')
-plt.grid()
-plt.plot(t_array, x_array, label='x (m)')
-plt.plot(t_array, v_array, label='v (m/s)')
-plt.legend()
-plt.show()
+    # Integration
+    for t in t_array:
+
+        # append current state to trajectories
+        euler_x_list.append(x_euler)
+        euler_v_list.append(v_euler)
+        verlet_x_list.append(x_verlet)
+
+        # calculate new euler position and velocity
+        a_euler = -k * x_euler / m
+        x_euler = x_euler + dt * v_euler
+        v_euler = v_euler + dt * a_euler
+
+        # calculate new verlet postitions
+        a_verlet = -k * x_verlet / m
+        if t == 0:
+            x_prev = x_verlet
+            x_verlet = x_verlet + dt * v_verlet
+        else:
+            temp = x_verlet
+            x_verlet = 2*x_verlet - x_prev + dt*dt*a_verlet
+            x_prev = temp
+
+    # calculate verlet velocities from positions
+    for i in range(1, len(verlet_x_list)-1):
+        verlet_v_list.append(
+            (verlet_x_list[i+1]-verlet_x_list[i-1])/(2*dt)
+        )
+    verlet_v_list.append(
+        (verlet_x_list[-1]-verlet_x_list[-2])/dt
+    )
+
+    print(verlet_x_list[0], verlet_v_list[0],
+          t_array[0], euler_v_list[0], euler_x_list[0])
+
+    # convert trajectory lists into arrays, so they can be sliced (useful for Assignment 2)
+    euler_x_array = np.array(euler_x_list)
+    euler_v_array = np.array(euler_v_list)
+    x_array = np.array(verlet_x_list)
+    v_array = np.array(verlet_v_list)
+
+    # plot the position-time graph
+    fig, ax = plt.subplots()
+    plt.xlabel('time (s)')
+    # ax.plot(t_array, euler_x_array, label='Euler')
+    ax.plot(t_array, x_array, label='Verlet')
+    ax.plot(t_array, B*np.sin(omega*t_array), label='Analytical')
+    plt.title('Position-time graph')
+    plt.legend()
+
+    fig, ax = plt.subplots()
+    plt.xlabel('time (s)')
+    plt.ylabel('velocity (m\s)')
+    # ax.plot(t_array, euler_v_array, label='Euler')
+    ax.plot(t_array, v_array, label='Verlet')
+    ax.plot(t_array, np.cos(omega*t_array), label='Analytical')
+    plt.title('Velocity-time graph')
+    ax.legend()
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
