6c9047f101d7aacf7308e76349e3ccfa8014bd9e
Lucius Bligh <lucius@lucius.org.uk>
2021-07-25 18:50:07 +0100

    refactored lander.h so vscode didnt complain

--- lander.h
+++ lander.h
@@ -32,7 +32,7 @@
 #include <cstdlib>
 
 // GLUT mouse wheel operations work under Linux only
-#if !defined (GLUT_WHEEL_UP)
+#if !defined(GLUT_WHEEL_UP)
 #define GLUT_WHEEL_UP 3
 #define GLUT_WHEEL_DOWN 4
 #endif
@@ -60,80 +60,147 @@
 
 // Mars constants
 #define MARS_RADIUS 3386000.0 // (m)
-#define MARS_MASS 6.42E23 // (kg)
-#define GRAVITY 6.673E-11 // (m^3/kg/s^2)
-#define MARS_DAY 88642.65 // (s)
-#define EXOSPHERE 200000.0 // (m)
+#define MARS_MASS 6.42E23     // (kg)
+#define GRAVITY 6.673E-11     // (m^3/kg/s^2)
+#define MARS_DAY 88642.65     // (s)
+#define EXOSPHERE 200000.0    // (m)
 
 // Lander constants
-#define LANDER_SIZE 1.0 // (m)
-#define UNLOADED_LANDER_MASS 100.0 // (kg)
-#define FUEL_CAPACITY 100.0 // (l)
+#define LANDER_SIZE 1.0             // (m)
+#define UNLOADED_LANDER_MASS 100.0  // (kg)
+#define FUEL_CAPACITY 100.0         // (l)
 #define FUEL_RATE_AT_MAX_THRUST 0.5 // (l/s)
-#define FUEL_DENSITY 1.0 // (kg/l)
+#define FUEL_DENSITY 1.0            // (kg/l)
 // MAX_THRUST, as defined below, is 1.5 * weight of fully loaded lander at surface
-#define MAX_THRUST (1.5 * (FUEL_DENSITY*FUEL_CAPACITY+UNLOADED_LANDER_MASS) * (GRAVITY*MARS_MASS/(MARS_RADIUS*MARS_RADIUS))) // (N)
-#define ENGINE_LAG 0.0 // (s)
-#define ENGINE_DELAY 0.0 // (s)
+#define MAX_THRUST (1.5 * (FUEL_DENSITY * FUEL_CAPACITY + UNLOADED_LANDER_MASS) * (GRAVITY * MARS_MASS / (MARS_RADIUS * MARS_RADIUS))) // (N)
+#define ENGINE_LAG 0.0                                                                                                                 // (s)
+#define ENGINE_DELAY 0.0                                                                                                               // (s)
 #define DRAG_COEF_CHUTE 2.0
 #define DRAG_COEF_LANDER 1.0
-#define MAX_PARACHUTE_DRAG 20000.0 // (N)
-#define MAX_PARACHUTE_SPEED 500.0 // (m/s)
-#define THROTTLE_GRANULARITY 20 // for manual control
+#define MAX_PARACHUTE_DRAG 20000.0  // (N)
+#define MAX_PARACHUTE_SPEED 500.0   // (m/s)
+#define THROTTLE_GRANULARITY 20     // for manual control
 #define MAX_IMPACT_GROUND_SPEED 1.0 // (m/s)
 #define MAX_IMPACT_DESCENT_RATE 1.0 // (m/s)
 
 using namespace std;
 
-class vector3d {
+class vector3d
+{
   // Utility class for three-dimensional vector operations
 public:
-  vector3d() {x=0.0; y=0.0; z=0.0;}
-  vector3d (double a, double b, double c=0.0) {x=a; y=b; z=c;}
-  bool operator== (const vector3d &v) const { if ((x==v.x)&&(y==v.y)&&(z==v.z)) return true; else return false; }
-  bool operator!= (const vector3d &v) const { if ((x!=v.x)||(y!=v.y)||(z!=v.z)) return true; else return false; }
-  vector3d operator+ (const vector3d &v) const { return vector3d(x+v.x, y+v.y, z+v.z); }
-  vector3d operator- (const vector3d &v) const { return vector3d(x-v.x, y-v.y, z-v.z); }
-  friend vector3d operator- (const vector3d &v) { return vector3d(-v.x, -v.y, -v.z); }
-  vector3d& operator+= (const vector3d &v) { x+=v.x; y+=v.y; z+=v.z; return *this; }
-  vector3d& operator-= (const vector3d &v) { x-=v.x; y-=v.y; z-=v.z; return *this; }
-  vector3d operator^ (const vector3d &v) const { return vector3d(y*v.z-z*v.y, z*v.x-x*v.z, x*v.y-y*v.x); }
-  double operator* (const vector3d &v) const { return (x*v.x + y*v.y +z*v.z); }
-  friend vector3d operator* (const vector3d &v, const double &a) { return vector3d(v.x*a, v.y*a, v.z*a); }
-  friend vector3d operator* (const double &a, const vector3d &v) { return vector3d(v.x*a, v.y*a, v.z*a); }
-  vector3d& operator*= (const double &a) { x*=a; y*=a; z*=a; return *this; }
-  vector3d operator/ (const double &a) const { return vector3d(x/a, y/a, z/a); }
-  vector3d& operator/= (const double &a) { x/=a; y/=a; z/=a; return *this; }
-  double abs2() const { return (x*x + y*y + z*z); }
+  vector3d()
+  {
+    x = 0.0;
+    y = 0.0;
+    z = 0.0;
+  }
+  vector3d(double a, double b, double c = 0.0)
+  {
+    x = a;
+    y = b;
+    z = c;
+  }
+  bool operator==(const vector3d &v) const
+  {
+    if ((x == v.x) && (y == v.y) && (z == v.z))
+      return true;
+    else
+      return false;
+  }
+  bool operator!=(const vector3d &v) const
+  {
+    if ((x != v.x) || (y != v.y) || (z != v.z))
+      return true;
+    else
+      return false;
+  }
+  vector3d operator+(const vector3d &v) const { return vector3d(x + v.x, y + v.y, z + v.z); }
+  vector3d operator-(const vector3d &v) const { return vector3d(x - v.x, y - v.y, z - v.z); }
+  friend vector3d operator-(const vector3d &v) { return vector3d(-v.x, -v.y, -v.z); }
+  vector3d &operator+=(const vector3d &v)
+  {
+    x += v.x;
+    y += v.y;
+    z += v.z;
+    return *this;
+  }
+  vector3d &operator-=(const vector3d &v)
+  {
+    x -= v.x;
+    y -= v.y;
+    z -= v.z;
+    return *this;
+  }
+  vector3d operator^(const vector3d &v) const { return vector3d(y * v.z - z * v.y, z * v.x - x * v.z, x * v.y - y * v.x); }
+  double operator*(const vector3d &v) const { return (x * v.x + y * v.y + z * v.z); }
+  friend vector3d operator*(const vector3d &v, const double &a) { return vector3d(v.x * a, v.y * a, v.z * a); }
+  friend vector3d operator*(const double &a, const vector3d &v) { return vector3d(v.x * a, v.y * a, v.z * a); }
+  vector3d &operator*=(const double &a)
+  {
+    x *= a;
+    y *= a;
+    z *= a;
+    return *this;
+  }
+  vector3d operator/(const double &a) const { return vector3d(x / a, y / a, z / a); }
+  vector3d &operator/=(const double &a)
+  {
+    x /= a;
+    y /= a;
+    z /= a;
+    return *this;
+  }
+  double abs2() const { return (x * x + y * y + z * z); }
   double abs() const { return sqrt(this->abs2()); }
-  vector3d norm() const { double s(this->abs()); if (s==0) return *this; else return vector3d(x/s, y/s, z/s); }
-  friend ostream& operator << (ostream &out, const vector3d &v) { out << v.x << ' ' << v.y << ' ' << v.z; return out; }
+  vector3d norm() const
+  {
+    double s(this->abs());
+    if (s == 0)
+      return *this;
+    else
+      return vector3d(x / s, y / s, z / s);
+  }
+  friend ostream &operator<<(ostream &out, const vector3d &v)
+  {
+    out << v.x << ' ' << v.y << ' ' << v.z;
+    return out;
+  }
   double x, y, z;
+
 private:
 };
 
 // Data type for recording lander's previous positions
-struct track_t {
+struct track_t
+{
   unsigned short n;
   unsigned short p;
   vector3d pos[N_TRACK];
 };
 
 // Quaternions for orbital view transformation
-struct quat_t {
+struct quat_t
+{
   vector3d v;
   double s;
 };
 
 // Data structure for the state of the close-up view's coordinate system
-struct closeup_coords_t {
+struct closeup_coords_t
+{
   bool initialized;
   bool backwards;
   vector3d right;
 };
 
 // Enumerated data type for parachute status
-enum parachute_status_t { NOT_DEPLOYED = 0, DEPLOYED = 1, LOST = 2 };
+enum parachute_status_t
+{
+  NOT_DEPLOYED = 0,
+  DEPLOYED = 1,
+  LOST = 2
+};
 
 #ifdef DECLARE_GLOBAL_VARIABLES // actual declarations of all global variables for lander_graphics.cpp
 
@@ -164,7 +231,7 @@ unsigned long throttle_buffer_length, throttle_buffer_pointer;
 double *throttle_buffer = NULL;
 unsigned long long time_program_started;
 
-// Lander state - the visualization routines use velocity_from_positions, so not sensitive to 
+// Lander state - the visualization routines use velocity_from_positions, so not sensitive to
 // any errors in the velocity update in numerical_dynamics
 vector3d position, orientation, velocity, velocity_from_positions, last_position;
 double climb_speed, ground_speed, altitude, throttle, fuel;
@@ -177,11 +244,11 @@ double orbital_zoom, save_orbital_zoom, closeup_offset, closeup_xr, closeup_yr, 
 quat_t orbital_quat;
 
 // For GL lights
-GLfloat plus_y[] = { 0.0, 1.0, 0.0, 0.0 };
-GLfloat minus_y[] = { 0.0, -1.0, 0.0, 0.0 };
-GLfloat plus_z[] = { 0.0, 0.0, 1.0, 0.0 };
-GLfloat top_right[] = { 1.0, 1.0, 1.0, 0.0 };
-GLfloat straight_on[] = { 0.0, 0.0, 1.0, 0.0 };
+GLfloat plus_y[] = {0.0, 1.0, 0.0, 0.0};
+GLfloat minus_y[] = {0.0, -1.0, 0.0, 0.0};
+GLfloat plus_z[] = {0.0, 0.0, 1.0, 0.0};
+GLfloat top_right[] = {1.0, 1.0, 1.0, 0.0};
+GLfloat straight_on[] = {0.0, 0.0, 1.0, 0.0};
 
 #else // extern declarations of those global variables used in lander.cpp
 
@@ -196,53 +263,53 @@ extern int stabilized_attitude_angle;
 #endif
 
 // Function prototypes
-void invert (double m[], double mout[]);
-void xyz_euler_to_matrix (vector3d ang, double m[]);
-vector3d matrix_to_xyz_euler (double m[]);
-void normalize_quat (quat_t &q);
-quat_t axis_to_quat (vector3d a, const double phi);
-double project_to_sphere (const double r, const double x, const double y);
-quat_t add_quats (quat_t q1, quat_t q2);
-void quat_to_matrix (double m[], const quat_t q);
-quat_t track_quats (const double p1x, const double p1y, const double p2x, const double p2y);
-void microsecond_time (unsigned long long &t);
-void fghCircleTable (double **sint, double **cost, const int n);
-void glutOpenHemisphere (GLdouble radius, GLint slices, GLint stacks);
-void glutMottledSphere (GLdouble radius, GLint slices, GLint stacks);
-void glutCone (GLdouble base, GLdouble height, GLint slices, GLint stacks, bool closed);
-void enable_lights (void);
-void setup_lights (void);
-void glut_print (float x, float y, string s);
-double atmospheric_density (vector3d pos);
-void draw_dial (double cx, double cy, double val, string title, string units);
-void draw_control_bar (double tlx, double tly, double val, double red, double green, double blue, string title);
-void draw_indicator_lamp (double tcx, double tcy, string off_text, string on_text, bool on);
-void draw_instrument_window (void);
-void display_help_arrows (void);
-void display_help_prompt (void);
-void display_help_text (void);
-void draw_orbital_window (void);
-void draw_parachute_quad (double d);
-void draw_parachute (double d);
-bool generate_terrain_texture (void);
-void update_closeup_coords (void);
-void draw_closeup_window (void);
-void draw_main_window (void);
-void refresh_all_subwindows (void);
-bool safe_to_deploy_parachute (void);
-void update_visualization (void);
-void attitude_stabilization (void);
-vector3d thrust_wrt_world (void);
-void autopilot (void);
-void numerical_dynamics (void);
-void initialize_simulation (void);
-void update_lander_state (void);
-void reset_simulation (void);
-void set_orbital_projection_matrix (void);
-void reshape_main_window (int width, int height);
-void orbital_mouse_button (int button, int state, int x, int y);
-void orbital_mouse_motion (int x, int y);
-void closeup_mouse_button (int button, int state, int x, int y);
-void closeup_mouse_motion (int x, int y);
-void glut_special (int key, int x, int y);
-void glut_key (unsigned char k, int x, int y);
+void invert(double m[], double mout[]);
+void xyz_euler_to_matrix(vector3d ang, double m[]);
+vector3d matrix_to_xyz_euler(double m[]);
+void normalize_quat(quat_t &q);
+quat_t axis_to_quat(vector3d a, const double phi);
+double project_to_sphere(const double r, const double x, const double y);
+quat_t add_quats(quat_t q1, quat_t q2);
+void quat_to_matrix(double m[], const quat_t q);
+quat_t track_quats(const double p1x, const double p1y, const double p2x, const double p2y);
+void microsecond_time(unsigned long long &t);
+void fghCircleTable(double **sint, double **cost, const int n);
+void glutOpenHemisphere(GLdouble radius, GLint slices, GLint stacks);
+void glutMottledSphere(GLdouble radius, GLint slices, GLint stacks);
+void glutCone(GLdouble base, GLdouble height, GLint slices, GLint stacks, bool closed);
+void enable_lights(void);
+void setup_lights(void);
+void glut_print(float x, float y, string s);
+double atmospheric_density(vector3d pos);
+void draw_dial(double cx, double cy, double val, string title, string units);
+void draw_control_bar(double tlx, double tly, double val, double red, double green, double blue, string title);
+void draw_indicator_lamp(double tcx, double tcy, string off_text, string on_text, bool on);
+void draw_instrument_window(void);
+void display_help_arrows(void);
+void display_help_prompt(void);
+void display_help_text(void);
+void draw_orbital_window(void);
+void draw_parachute_quad(double d);
+void draw_parachute(double d);
+bool generate_terrain_texture(void);
+void update_closeup_coords(void);
+void draw_closeup_window(void);
+void draw_main_window(void);
+void refresh_all_subwindows(void);
+bool safe_to_deploy_parachute(void);
+void update_visualization(void);
+void attitude_stabilization(void);
+vector3d thrust_wrt_world(void);
+void autopilot(void);
+void numerical_dynamics(void);
+void initialize_simulation(void);
+void update_lander_state(void);
+void reset_simulation(void);
+void set_orbital_projection_matrix(void);
+void reshape_main_window(int width, int height);
+void orbital_mouse_button(int button, int state, int x, int y);
+void orbital_mouse_motion(int x, int y);
+void closeup_mouse_button(int button, int state, int x, int y);
+void closeup_mouse_motion(int x, int y);
+void glut_special(int key, int x, int y);
+void glut_key(unsigned char k, int x, int y);
